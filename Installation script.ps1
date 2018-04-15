7405eb4b03f39b539f24688e4d9870f9ac07aa7f
Ravid <ravid12@gmail.com>
2018-04-15 12:13:38 +1200

    Created Script

---
+++ Installation script.ps1
@@ -0,0 +1,18 @@
+function Reload-Path
+{
+    $env:Path = "$([System.Environment]::GetEnvironmentVariable("Path","Machine"));$([System.Environment]::GetEnvironmentVariable("Path","User"))"
+    $env:JAVA_HOME = "$([System.Environment]::GetEnvironmentVariable("JAVA_HOME","Machine"));$([System.Environment]::GetEnvironmentVariable("JAVA_HOME","User"))"
+}
+
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+Reload-Path
+& choco install --yes `
+	spotify `
+	discord `
+	steam `
+	notepadplusplus `
+	googlechrome `
+	f.lux `
+	paint.net `
+	winrar `
